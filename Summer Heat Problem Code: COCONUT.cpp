e965e8d918570c6daaf9fa12ddcf54e680ccffa9
shubham1708 <61815446+shubham1708@users.noreply.github.com>
2021-06-07 09:49:57 +0530

    Create Summer Heat Problem Code: COCONUT.cpp
    Shubham Yadav
    Manav Rachna University
    CodeChef june long challange 2021 DIV 2
    Summer heat solution

---
+++ Summer Heat Problem Code: COCONUT.cpp
@@ -0,0 +1,16 @@
+//Shubham Yadav
+//Manav Rachna University
+//CodeChef june long challange 2021 DIV 2
+//Summer heat solution
+
+
+#include<iostream>
+using namespace std;
+#define ll long long
+int main(){
+    ll t;  cin>>t;
+    while(t--){
+    int a,b,c,d;  cin>>a>>b>>c>>d;
+    cout<<c/a+d/b<<endl;
+    }
+}
